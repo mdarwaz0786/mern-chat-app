f22fd55c775ed1e275cdde85452dc079a9e5c0e7
Md Arwaz <mdarwaznew2017@gmail.com>
2024-03-10 22:12:46 +0530

    make responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,29 @@
 import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 768); // Adjust the breakpoint according to your design
+		};
+
+		handleResize(); // Initial check
+
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-lg'>
+		<div className={`flex ${isMobile ? 'flex-col' : 'sm:flex-row'} h-[95vh] rounded-lg overflow-hidden bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-lg`}>
 			<Sidebar />
 			<MessageContainer />
 		</div>
 	);
 };
+
 export default Home;
